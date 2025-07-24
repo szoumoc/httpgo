4b3cbf93ecc7387eca7eb164c19065b3919de1ed
szoumoc <soumosaha4327@gmail.com>
2025-07-24 21:46:31 +0530

    INIT: First commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	Name string `json:"name"`
+}
+
+var userCache = make(map[int]User)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRoot)
+	
+	mux.HandleFunc("POST /users", createUser)
+
+	fmt.Println("Server is running on port 4032")
+	http.ListenAndServe(":4032", mux)
+}
+
+func handleRoot(w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if user.Name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
+	}
+
+
+
+	userCache[len(userCache)+1] = user
+	w.WriteHeader(http.StatusNoContent)
+}
